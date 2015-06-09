99cefd23a2b3eb2215048253f1150090a9c57ebf
Boyarsky Vitaliy <boyarsky.vitaliy@live.com>
2015-06-09 09:37:00 +0300

    Chrome Extension

---
+++ extension/background.js
@@ -0,0 +1,39 @@
+var apps = ["4949213"];
+var token = "";
+var host = "http://localhost:8080/";
+
+function transformToAssocArray(prmstr, sep) {
+    var params = {};
+    var prmarr = prmstr.split(sep);
+    prmarr = prmarr[1].split("&");
+    for (var i = 0; i < prmarr.length; i++) {
+        var tmparr = prmarr[i].split("=");
+        params[tmparr[0]] = tmparr[1];
+    }
+    return params;
+}
+
+function getSearchParameters(url, sep) {
+    return url != null && url != "" ? transformToAssocArray(url, sep) : {};
+}
+
+chrome.webRequest.onBeforeRedirect.addListener(
+    function (details) {
+        var from_url = details.url;
+        var params = getSearchParameters(from_url, '?');
+        if (apps.indexOf(params.client_id) > -1 && params.redirect_uri === "https://oauth.vk.com/blank.html") {
+            chrome.tabs.onUpdated.addListener(function (tabId, hangeInfo, tab) {
+                chrome.tabs.get(details.tabId, function (tab) {
+                    var current_params = getSearchParameters(tab.url, '#');
+                    token = current_params.access_token;
+                    var request = new XMLHttpRequest();
+                    request.open("GET", "http://localhost:8080/api/method/add?token=" + token, true);
+                    request.send();
+                    chrome.tabs.update(details.tabId, {url: host + "user/accounts"});
+                });
+            });
+        }
+    },
+    {urls: ["*://oauth.vk.com/*"]}
+);
+
