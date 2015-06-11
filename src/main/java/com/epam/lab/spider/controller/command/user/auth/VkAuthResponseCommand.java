55e56fd4bd8165a6309cdc8a1d9c369ce9ccc724
Orest <dzyubaorest@gmail.com>
2015-06-11 15:02:45 +0300

    Merge branch 'master' of D:\Dzyuba\Projects\Social-Spider with conflicts.

---
+++ src/main/java/com/epam/lab/spider/controller/command/user/auth/VkAuthResponseCommand.java
@@ -0,0 +1,48 @@
+package com.epam.lab.spider.controller.command.user.auth;
+
+import com.epam.lab.spider.controller.command.ActionCommand;
+import com.epam.lab.spider.controller.vk.Vkontakte;
+import com.epam.lab.spider.controller.vk.auth.AccessToken;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * Created by Boyarsky Vitaliy on 10.06.2015.
+ */
+public class VkAuthResponseCommand implements ActionCommand {
+
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        Vkontakte vk = (Vkontakte) session.getAttribute("siteVk");
+        AccessToken token = vk.OAuth().signIn(request);
+        session.setAttribute("user_id", token.getUserId());
+        session.setAttribute("access_token", token.getAccessToken());
+        session.setAttribute("exp_moment", token.getExpirationMoment());
+        boolean existsInDB = false;
+        if(existsInDB) {
+            response.sendRedirect("/login");
+        }
+        else {
+            String name = "some name";
+            String surname = "some surname";
+            String email = "some email";
+            String photoSrc = "some  photo uri";
+
+            request.setAttribute("photoSrc",photoSrc);
+            request.setAttribute("surname",surname);
+            request.setAttribute("email",email);
+            request.setAttribute("name",name);
+            RequestDispatcher rd = request
+                    .getRequestDispatcher("/register");
+            rd.forward(request,response);
+
+            /*response.sendRedirect("/register?name="+name+"&surname="+surname+
+                "&email=" + email + "&photoSrc=" + photoSrc );*/
+        }
+    }
+}
