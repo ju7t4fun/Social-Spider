9bdbfbef296f81d1730305f74aee970f358f9edb
Marian Voronovskyi <marian.voronovskyi@gmail.com>
2015-06-12 11:57:03 +0300

    added send mail servlet, mail encryptor, send mail class

--- src/main/java/com/epam/lab/spider/controller/servlet/SendMailServlet.java
+++ src/main/java/com/epam/lab/spider/controller/servlet/SendMailServlet.java
@@ -1,5 +1,6 @@
 package com.epam.lab.spider.controller.servlet;
 
+import com.epam.lab.spider.controller.utils.MailEncryptor;
 import com.epam.lab.spider.controller.utils.SendEmail;
 
 import javax.servlet.ServletException;
@@ -7,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
 /**
  * Created by Marian Voronovskyi on 12.06.2015.
@@ -20,7 +22,10 @@ public class SendMailServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        SendEmail.send("marik1993@i.ua", "title", "<p><a href=\"http://google.com\">Google</a></p>");
+        SendEmail.send("marik1993@i.ua", "Email confirm", "<p><a href=\"http://localhost:8080/spider?" +
+                "param=" + new Date().getTime() + MailEncryptor.encoding("marik1993@i.ua") + "\">" +
+                "Please click here to " +
+                "confirm your account</a></p>");
         request.getRequestDispatcher("/").forward(request,
                 response);
     }
