e129df9f74d698f3d2782a91c3da80615bcb282b
Marian Voronovskyi <marian.voronovskyi@gmail.com>
2015-06-12 11:07:29 +0300

    added send mail servlet, mail encryptor, send mail class

---
+++ src/main/java/com/epam/lab/spider/controller/servlet/SendMailServlet.java
@@ -0,0 +1,27 @@
+package com.epam.lab.spider.controller.servlet;
+
+import com.epam.lab.spider.controller.utils.SendEmail;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Marian Voronovskyi on 12.06.2015.
+ */
+public class SendMailServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+                          throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+                          throws ServletException, IOException {
+
+        SendEmail.send("marik1993@i.ua", "title", "<p><a href=\"http://google.com\">Google</a></p>");
+        request.getRequestDispatcher("/").forward(request,
+                response);
+    }
+}
---
+++ src/main/java/com/epam/lab/spider/controller/utils/MailEncryptor.java
@@ -0,0 +1,24 @@
+package com.epam.lab.spider.controller.utils;
+
+/**
+ * Created by Marian Voronovskyi on 12.06.2015.
+ */
+public class MailEncryptor {
+
+    public static String encoding(String in) {
+        StringBuilder builder = new StringBuilder();
+        for (char c : in.toCharArray()) {
+            builder.append(String.format("%02x", (int) c));
+        }
+        return builder.toString();
+    }
+
+    public static String decoding(String in) {
+        StringBuilder builder = new StringBuilder();
+        char [] chars = in.toCharArray();
+        for (int i=0; i<in.length();  i=i+2 ) {
+            builder.append((char) Integer.parseInt("" +  chars[i] + chars[i +1], 16));
+        }
+        return builder.toString();
+    }
+}
