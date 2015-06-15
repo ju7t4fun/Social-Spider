b3c48b9e63d1c46c6be3edc2f1a0d7369160c07b
Orest <dzyubaorest@gmail.com>
2015-06-15 12:21:30 +0300

    send restore command

--- src/main/java/com/epam/lab/spider/Run.java
+++ src/main/java/com/epam/lab/spider/Run.java
@@ -7,6 +7,7 @@ import com.epam.lab.spider.model.entity.User;
 import com.epam.lab.spider.model.service.CategoryService;
 import com.epam.lab.spider.model.service.ServiceFactory;
 import com.epam.lab.spider.model.service.UserService;
+import org.apache.commons.codec.binary.Base64;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -17,15 +18,23 @@ import java.util.List;
 public class Run {
 
     public static void main(String[] args) throws SQLException {
-       ServiceFactory factory = ServiceFactory.getInstance();
+      /* ServiceFactory factory = ServiceFactory.getInstance();
         UserService service = factory.create(UserService.class);
         User user = new User();
-        user.setName("виик");
-        user.setSurname("хуйло");
+        user.setName("пїЅпїЅпїЅпїЅ");
+        user.setSurname("пїЅпїЅпїЅпїЅпїЅ");
         user.setEmail("fsdfsdf");
         user.setPassword("dfsdfsdf");
         user.setRole(User.Role.ADMIN);
-        service.insert(user);
+        service.insert(user);*/
+        // encode data on your side using BASE64
+        String str = "HELLO AZAZA";
+        byte[]   bytesEncoded = Base64.encodeBase64(str.getBytes());
+        System.out.println("ecncoded value is " + new String(bytesEncoded ));
+
+        // Decode data on other side, by processing encoded data
+        byte[] valueDecoded= Base64.decodeBase64(bytesEncoded );
+        System.out.println("Decoded value is " + new String(valueDecoded));
 
     }
 
---
+++ src/main/java/com/epam/lab/spider/controller/command/user/auth/RetrieveRestoreCommand.java
@@ -0,0 +1,7 @@
+package com.epam.lab.spider.controller.command.user.auth;
+
+/**
+ * Created by Dmytro on 15.06.2015.
+ */
+public class RetrieveRestoreCommand {
+}
