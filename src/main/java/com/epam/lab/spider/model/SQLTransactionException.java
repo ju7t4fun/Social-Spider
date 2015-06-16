3322906ce5c64df35c7e67ba7e8f06284b8f90ee
Boyarsky Vitaliy <boyarsky.vitaliy@live.com>
2015-06-16 03:17:29 +0300

    added SQLTransactionException

---
+++ src/main/java/com/epam/lab/spider/model/SQLTransactionException.java
@@ -0,0 +1,10 @@
+package com.epam.lab.spider.model;
+
+import java.sql.SQLException;
+
+/**
+ * Created by Boyarsky Vitaliy on 16.06.2015.
+ */
+public class SQLTransactionException extends SQLException {
+
+}
