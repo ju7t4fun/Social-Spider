bf490bf8f0fda8c053377f834c080d6456b1c35d
Marian Voronovskyi <marian.voronovskyi@gmail.com>
2015-06-16 20:33:27 +0300

    updated UploadImageController updated fileinput.css updated addPost.jsp

--- src/main/webapp/js/language.js
+++ src/main/webapp/js/language.js
@@ -1,20 +1,21 @@
-$( document ).ready(function() {
-    $(".language-button-logic").click(function(){
+$(document).ready(function () {
+    $(".language-button-logic").click(function () {
         var lang = $(this).attr("change");
-        var names =[];i=0;
-        $(".loc-t, .loc-p").each(function(){
-            names[i++]=$(this).attr("locres")
+        var names = [];
+        i = 0;
+        $(".loc-t, .loc-p").each(function () {
+            names[i++] = $(this).attr("locres")
         });
 
         var myJsonString = JSON.stringify(names);
-        $.post( "controller?action=locale&lang=".concat(lang),{ names:myJsonString})
-            .done(function( data ) {
+        $.post("controller?action=locale&lang=".concat(lang), {names: myJsonString})
+            .done(function (data) {
                 var map = data;
-                $(".loc-t").each(function(){
+                $(".loc-t").each(function () {
                     $(this).text(map[$(this).attr("locres")]);
                 });
-                $(".loc-p").each(function(){
-                    $(this).attr("placeholder",map[$(this).attr("locres")]);
+                $(".loc-p").each(function () {
+                    $(this).attr("placeholder", map[$(this).attr("locres")]);
                 })
             });
     })
