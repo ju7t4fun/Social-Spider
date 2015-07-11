de9d0814459f289fdefc5bc0dc7a4c2530888f36
Marian Voronovskyi <marian.voronovskyi@gmail.com>
2015-07-11 18:11:35 +0300

    fixed registration valid

--- src/main/webapp/js/form-validation-script.js
+++ src/main/webapp/js/form-validation-script.js
@@ -1,12 +1,6 @@
 var Script = function () {
-
     $().ready(function() {
-        // validate the comment form when it is submitted
-        $("#feedback_form").validate();
-
-        // validate signup form on keyup and submit
         $("#register_form").validate({
-
             rules: {
                 name: {
                     required: true,
@@ -66,21 +60,16 @@ var Script = function () {
                 },
                 email: "Please enter a valid email address.",
                 agree: "Please accept our terms & condition."
+            },
+            errorPlacement: function (error, element) {
+                element.attr('title', error.text());
+                $(".error").tooltip(
+                    {
+                        tooltipClass: "mytooltip",
+                        placement:'top',
+                        html:true
+                    });
             }
         });
-
-        //code to hide topic selection, disable for demo
-        var newsletter = $("#newsletter");
-        // newsletter topics are optional, hide at first
-        var inital = newsletter.is(":checked");
-        var topics = $("#newsletter_topics")[inital ? "removeClass" : "addClass"]("gray");
-        var topicInputs = topics.find("input").attr("disabled", !inital);
-        // show when newsletter is checked
-        newsletter.click(function() {
-            topics[this.checked ? "removeClass" : "addClass"]("gray");
-            topicInputs.attr("disabled", !this.checked);
-        });
     });
-
-
 }();
